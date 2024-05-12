6f6accbfdf0823f1513511dcb7b1cbf9473dce0d
Abeer Iqbal <abeeriqbalahmed@gmail.com>
2024-05-12 16:52:58 +0500

    create files

---
+++ app.js
@@ -0,0 +1,53 @@
+///* To do app *///
+// get value from user using input
+// create para in div with js
+// append to in pera using get div html
+// create buttn by js
+// name of delete and add 
+// every para delete should have btn
+
+var parent = document.getElementById("parent");
+var inp = document.getElementById("inp");
+
+function del(delBtn) {
+        console.log(delBtn.parentNode);
+        delBtn.parentElement.remove();
+}
+
+function delAll(Button) {
+        parent.innerHTML = "";
+        inp.value = "";
+}
+
+function edt(button) {
+        console.log(button.parentNode.childNodes[0]);
+        var newText = prompt("Enter new text:");
+        if (newText !== null) {
+            button.parentNode.childNodes[0].nodeValue = newText;
+        }
+    }
+    
+
+function addElement() {
+        var pera = document.createElement("p");
+        var text = document.createTextNode(inp.value);
+        pera.appendChild(text);
+        
+        
+        var editBtn = document.createElement("button");
+        var editText = document.createTextNode("Edit");
+        editBtn.appendChild(editText);
+        editBtn.setAttribute("onclick", "edt(this)");
+
+        var delBtn = document.createElement("button");
+        var deletText = document.createTextNode("Delete");
+        delBtn.appendChild(deletText);
+        delBtn.onclick = function() {
+                del(this);
+            };
+        
+        pera.appendChild(delBtn); 
+        pera.appendChild(editBtn);
+        parent.appendChild(pera); 
+        console.log(pera)
+}
